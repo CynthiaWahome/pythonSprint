0ba3fc96d3ef01f6c1095a5bbd43ea28f6e3fafa
ProgramKingAbel <abelmorara254@gmail.com>
2024-04-29 14:48:38 +0300

    label formatting types

--- chapter_1/personal_message.py
+++ chapter_1/personal_message.py
@@ -1,6 +1,6 @@
 name = "Cynthia"
 lastName = "Morara"
-print("Hello " + name + " " + lastName + " would you like to learn some python today")
-print(f"Hello {name} {lastName}, would you like to learn some python today stmt2")
-print("Hello", name, lastName, "Would you like to learn some python today stmt3")
-print("Hello {} {} would you like to learn more today stmt 4".format(name, lastName))
+print("Hello " + name + " " + lastName + " would you like to learn some python today") #concatenation
+print(f"Hello {name} {lastName}, would you like to learn some python today stmt2") # using f-strings
+print("Hello", name, lastName, "Would you like to learn some python today stmt3") #separating with a comma
+print("Hello {} {} would you like to learn more today stmt 4".format(name, lastName)) # using string formatting
