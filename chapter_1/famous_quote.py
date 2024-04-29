27bf93da465d2b4453befcaf966506efa99a4f98
ProgramKingAbel <abelmorara254@gmail.com>
2024-04-29 15:26:08 +0300

    escaping characters

---
+++ chapter_1/famous_quote.py
@@ -0,0 +1,5 @@
+print("\t\tCynthia Wahome once said, \"A person who never made a \n\t\tmistake nerver tried anything new.\"")
+
+print("\t\t\tHello World")
+print('"Hello world"')
+print("\"Hello world\"\"")
