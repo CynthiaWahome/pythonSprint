6d08e99bb6f5c19f238a263b57ae783e0b247561
ProgramKingAbel <abelmorara254@gmail.com>
2024-04-29 14:45:58 +0300

    feat: printing str fomarts

--- chapter_1/personal_message.py
+++ chapter_1/personal_message.py
@@ -1,3 +1,6 @@
 name = "Cynthia"
-print("Hello" + name +  "would you like to learn some python today")
-print(f"Hello {name}, would you like to learn some python today stmt2")
+lastName = "Morara"
+print("Hello " + name + " " + lastName + " would you like to learn some python today")
+print(f"Hello {name} {lastName}, would you like to learn some python today stmt2")
+print("Hello", name, lastName, "Would you like to learn some python today stmt3")
+print("Hello {} {} would you like to learn more today stmt 4".format(name, lastName))
