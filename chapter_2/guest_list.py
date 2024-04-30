d92c063919f1f6b302ca67609a6b108793047e15
ProgramKingAbel <abelmorara254@gmail.com>
2024-04-30 15:15:00 +0300

    implement list methods with guest list

---
+++ chapter_2/guest_list.py
@@ -0,0 +1,11 @@
+guests = ["Mario Jones", "Ian Mugambi", "Cynthia", "Effie"]
+print(guests[0], "welcome to Dinner")
+print(guests[1], "Karibu")
+print(guests[-1], "Cheki we kam")
+print(guests[-2], "Usiende mbali nami")
+
+print(guests[1], "cant make it")
+
+guests[1] = "John"
+
+print(guests[1], "welcome to the party")
