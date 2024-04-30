c2c71316f4ee2f2662b076f02da349efd04bd87d
CynthiaWahome <cynthiaawsajira@gmail.com>
2024-04-30 14:08:28 +0300

    added num.py

--- chapter_1/name_cases.py
+++ chapter_1/name_cases.py
@@ -3,3 +3,5 @@ capitalized_string = name.capitalize() #Capitalize the first word
 print(capitalized_string)
 print(name.lower())
 print(name.upper())
+print("Hello, " + name.title() + "!")
+
---
+++ chapter_1/num.py
@@ -0,0 +1,15 @@
+print(5+3)
+print(10-2)
+print(16/2)
+print(4*2)
+print(2*2**2)
+print(2**3)
+
+fav_num = "14"
+print("This is my favourite number " + fav_num)
+
+best_num = 23
+print(str(best_num) + " is my favouritest number!")
+
+worst_num = 99
+print(str(worst_num),"is my worst number")
