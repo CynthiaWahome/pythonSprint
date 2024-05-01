b182cbfadab5e6e499674d663e00cfac24c11700
ProgramKingAbel <abelmorara254@gmail.com>
2024-05-01 14:04:07 +0300

    organizing lists

---
+++ chapter_2/cities.py
@@ -0,0 +1,22 @@
+cities = ["Carribean", "Nairobi", "Kigali", "Dubai"]
+print(cities)
+
+# organize list in alphabetical order
+
+# sorted(cities)
+
+# print("Cities after sort", cities)
+
+# cities with sort"
+# cities.sort(reverse=True)
+# print(cities)
+
+#organize list in reverse alphabetical order 
+# print("Reverse alphabet")
+
+#Using reverse 
+cities.reverse()
+print(cities)
+
+# lenght of a list with len
+print("Length of cities is", len(cities))
