396bd9ee9ee29c261e27495bfdf4190a1e2004db
CynthiaWahome <cynthiaawsajira@gmail.com>
2024-05-01 21:10:28 +0300

    added pizza.py

---
+++ chapter_2/pizza.py
@@ -0,0 +1,11 @@
+# fav_pizza = ["Peperonni","BBQ","Hawaiyan","Deluxe"]
+# for i in fav_pizza:
+#     print(i)
+#     print("This is inside the loop")
+# print("This is outside the loop")
+
+fav_pizza = ["Peperonni","BBQ","Hawaiyan","Deluxe"]
+for i in fav_pizza:
+    print("I really like ",i)
+print("I love pizza so very much!!!")
+print("My worstest pizza is", fav_pizza[2])
