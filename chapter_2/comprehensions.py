5606a0ef3d3c9549841c9ccb9cc219b2c8f8a858
CynthiaWahome <cynthiaawsajira@gmail.com>
2024-05-06 15:35:29 +0300

    added an entry

--- chapter_2/comprehensions.py
+++ chapter_2/comprehensions.py
@@ -1,10 +1,19 @@
-#nums = []
+nums = []
 
-#for i in range(1, 11):
-#    nums.append(i**2)
+for i in range(0, 11):
+   nums.insert(i, i**2)
+   #nums.append(i**2)
 
-#nums = list(nums)
-#print(nums)
+# nums = list(nums)
+print(nums)
 
-squares = [num ** 2 for num in range(1, 11)]
-print(squares)
+# squares = [num ** 2 for num in range(1, 11)]
+# print(squares)
+
+
+
+
+
+# cubes = [num**3 for num in range(1,11)]
+# print(cubes)
+
