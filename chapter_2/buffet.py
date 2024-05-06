cdfa4dd06c2f0c54c177ce3ff38e0d773befe811
ProgramKingAbel <abelmorara254@gmail.com>
2024-05-06 16:29:17 +0300

    feat: implement tupples

---
+++ chapter_2/buffet.py
@@ -0,0 +1,7 @@
+foods = ("banana", "avocado", "samosa", "sausages")
+for food in foods:
+    print("We offer ", food)
+
+foods = ("orange", "Salad", "samosa", "sausages")
+for food in foods:
+    print(food)
---
+++ chapter_2/tupples.py
@@ -0,0 +1,7 @@
+dimensions = (100, 50)
+print(dimensions[0])
+#dimensions.append(20)
+print(dimensions)
+
+for dimension in dimensions:
+    print("this is dimension", dimension)
