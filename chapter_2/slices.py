82866e9c239720a948c7815163b2463130f192ae
CynthiaWahome <cynthiaawsajira@gmail.com>
2024-05-06 16:01:21 +0300

    created a slices.py file

---
+++ chapter_2/slices.py
@@ -0,0 +1,4 @@
+items = ["Window", "Door", "Roof", "Cupboard","Wardrobe","Mirror"]
+print("The first three items in the list are: ", items[:4])
+print("Three items from the middle of the list are: ", items[1:4])
+print("The last three items in the list are: ", items[-3:])
