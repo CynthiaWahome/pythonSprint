ec2957c9e8daee5545af7978dfdcf3b7f9ee0573
CynthiaWahome <cynthiaawsajira@gmail.com>
2024-05-07 16:32:58 +0300

    Implement lifeStages.py

---
+++ chapter_3/alienColors.py
@@ -0,0 +1,8 @@
+alien_color = "Green"
+if alien_color == "Green":
+    print("You earned 5 points!")
+elif alien_color == "Yellow":
+    print("You earned 10 points!")
+elif alien_color == "Red":
+    print("You earned 15 points!")
+
---
+++ chapter_3/moreConditionalTests.py
@@ -0,0 +1,7 @@
+food = "Spaghetti"
+print("Is food == 'Spaghetti'? I predict True.")
+print(food == "Spaghetti")
+
+print("\nIs food == 'Chips'? I predict False.")
+print(food == 'chips')
+
