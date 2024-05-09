dc0034833adbbca1a8f16462f2723ea1b64476ed
ProgramKingAbel <abelmorara254@gmail.com>
2024-05-09 15:54:00 +0300

    feat: person object with dictionaries

---
+++ chapter_4/person.py
@@ -0,0 +1,22 @@
+# xtics of a person 
+# name => Cynthia
+# gender => prefer not to say
+# height => 5'8
+
+person_1 = {
+        'name': 'Abel',
+        'gender': 'PnT',
+        'height': 5.8
+        }
+
+print(person_1)
+
+print(person_1['name'])
+
+person_1['shoe size'] = 38
+person_1['is_happy'] = True
+person_1['fav_fruit'] = 'Matunda'
+person_1['name'] = 'Cynthia Wahome'
+del person_1['gender']
+
+print(person_1)
