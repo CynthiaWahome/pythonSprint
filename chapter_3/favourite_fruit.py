a30b6ce10e1138b9585631a79109980d7c5110bf
CynthiaWahome <cynthiaawsajira@gmail.com>
2024-05-09 09:21:55 +0300

    Removed the else statement

--- chapter_3/favourite_fruit.py
+++ chapter_3/favourite_fruit.py
@@ -1,12 +1,10 @@
 favorite_fruits = ["Oranges", "Bananas", "Pineapples", "Dragon Fruits", "Mango"]
-fruit = "Grapes"
+fruit = "Bananas"
 
-if fruit == favorite_fruits[0]:
+if fruit == favorite_fruits[1]:
     print("I really like",fruit,"!")
 if fruit == favorite_fruits[2]:
     print("I really like",fruit,"!")
 if fruit == favorite_fruits[3]:
     print("I really like",fruit,"!")
-
-else:
     print("Iza joh! That fruit is not in the list")
