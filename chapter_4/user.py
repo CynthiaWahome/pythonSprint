98cbb6ec03622c77e542d1cdac73affef82b64a5
ProgramKingAbel <abelmorara254@gmail.com>
2024-05-09 16:14:11 +0300

    loops in dictionaries

---
+++ chapter_4/user.py
@@ -0,0 +1,12 @@
+user_1 = {
+        'username': 'Abel',
+        'first': 'Enrico',
+        'last': 'fermi'
+        }
+
+for key, value in user_1.items():
+    print("\nKey", key)
+    print("\nValue", value)
+
+for name, nameValue in user_1.items():
+    print("The key is", name , "\t the value is", nameValue)
