c5a90bd3ce564946dde2212a2b7ee085ef524f3d
ProgramKingAbel <abelmorara254@gmail.com>
2024-05-10 14:53:17 +0300

    Python input stmt

---
+++ chapter_5/input.py
@@ -0,0 +1,9 @@
+prompt = "If you tell us who you are, we can personalize the messages you see."
+prompt += "\nWhat is your first name? "
+
+name = input(prompt)
+
+print("\n Hello, " + name + "!")
+
+age = int(input("How old are you? "))
+print(type(age))
