6dd448c54ae18c664ee609bc17de942ea2609a3a
cynthia <cynthiaawsajira@gmail.com>
2024-05-27 23:03:40 +0300

    included an exception

--- chapter_8/additionCalculator.py
+++ chapter_8/additionCalculator.py
@@ -1,15 +1,17 @@
 
-while True:
+try:
+    while True:
         first_num = input("First number: ")
         second_num = input("Second number: ")
 
-
         answer = int((first_num + second_num))
     
-    except (TypeError, ValueError):
-        print("Please insert a number")
-
     else:
         print(answer)
+            
+except (ValueError):
+    print("Please insert a number")
+
+
 
 # STUCK page 119
